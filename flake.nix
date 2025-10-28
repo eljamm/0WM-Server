fb4158e929a8bff26477f1aedd45448e42cc0330
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-28 13:38:07 +0100

    Add optimized package set

--- flake.nix
+++ flake.nix
@@ -33,12 +33,26 @@
         projectPackages = pkgs.callPackage ./nix/packages/default.nix { inherit ocamlPackages; };
         projectTests = pkgs.callPackage ./nix/tests/default.nix { inherit pkgs ngipkgs; };
 
+        # nix build .#_0wm-server-optimized -L
+        projectPackagesOptimized =
+          with pkgs.lib;
+          mapAttrs' (name: value: nameValuePair (name + "-optimized") value) (
+            projectPackages.override {
+              ocamlPackages = ocamlPackages.overrideScope (
+                self: super: {
+                  ocaml = super.ocaml.override { flambdaSupport = true; };
+                }
+              );
+            }
+          );
+
         # Flake packages need to be derivations
         filterPkgs = attrs: with pkgs.lib; filterAttrs (n: isDerivation) attrs;
       in
       {
-        packages = filterPkgs (projectPackages // projectTests);
+        packages = filterPkgs (projectPackages // projectPackagesOptimized // projectTests);
 
+        # nix flake check
         checks = {
           inherit (projectPackages) _0wm-server;
           inherit (projectTests) test;
